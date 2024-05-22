4c3e9398d2b1a7ecd4abb004458ec6a1b9318f95
Salwa-mansour <alqadasisal@gmail.com>
2024-05-22 19:34:18 +0300

    Shader mode added but there is a bug when you turn normal mode after shader mode event listner for mouseleave still calling shader , event lister used to lestin to colored poients only to not blow up performance , need to remove event listner or refactor the code sequance.

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,37 +1,64 @@
 
 const screen =document.querySelector('.screen');
-const color = document.querySelector('#color');
-const clearBtn = document.querySelector('#clear');
-const fillBtn = document.querySelector('#fill');
-
+const controls = document.querySelector('.controls')
+const color = controls.querySelector('#color');
+const clearBtn = controls.querySelector('#clear');
+const fillBtn = controls.querySelector('#fill');
+const options = controls.querySelector('#options');
+const pointSize = controls.querySelector('#point-size');
 
 function generatePoints(){
+  
   let screenWidth = screen.offsetWidth;
   let screenHeight = screen.offsetHeight;
-  let xPoints =Math.round(screenWidth / 10) ;
-  let yPoints =Math.round(screenHeight / 10) ;
+  let diminutions =parseInt(pointSize.value) ;
+  console.log(diminutions)
+  let xPoints =Math.round(screenWidth / diminutions) ;
+  let yPoints =Math.round(screenHeight / diminutions) ;
   let points  = xPoints * yPoints;
   for(let i = 0 ; i < points  ;i++){
   const div = document.createElement('div');
   div.classList.add('point');
+  div.setAttribute("data-moved-on", 0);
+  div.style.width =`${diminutions}px`;
+  div.style.height =`${diminutions}px`;
   screen.appendChild(div);
 }
- console.log(xPoints)
+
 }
 
 function colorize(e){
-    console.log(e.target.classList)
-
+ 
     if(e.target.classList.contains('point')){
-    e.target.style.backgroundColor = color.value;
-    e.target.classList.add(`color-${color.value}`)
+        e.target.classList.add(`color`);
+    if(options.value.match('normal')){
+        e.target.style.backgroundColor = color.value;
+        console.log('normal',color.value)
+    }else if(options.value.match('shader')){
+        e.target.addEventListener('mouseleave',shaderColoring);
+        console.log('shader')
+    }
+   
+  //  console.log(e.target)
+    }
+ 
+}
+function shaderColoring(e){
+   
+     let movedOnCounter = parseInt(e.target.dataset.movedOn) /* how many times moved on */
+     if(movedOnCounter < 10){
+     this.dataset.movedOn = movedOnCounter + 1 ;
+     this.style.opacity = `${this.dataset.movedOn * 10 }%`;
+     this.style.backgroundColor = '#000';
+   
     }
 }
-function clearScreen(){
-  items =  screen.querySelectorAll('[class*="color"]');
-  console.log(items)
+function resetScreen(){
+const  items =  screen.querySelectorAll('[class*="color"]');
+
   items.forEach(item => {
     item.style.backgroundColor = 'transparent';
+    item.style.opacity = '100%';
   });
   screen.style.backgroundColor = '#fff';
 }
@@ -43,5 +70,10 @@ generatePoints();
 window.addEventListener('resize',generatePoints);
  screen.addEventListener('mousemove',colorize);
 //screen.addEventListener('mouseenter',colorize);
-clearBtn.addEventListener('click',clearScreen);
-fillBtn.addEventListener('click',colorScreen)
+clearBtn.addEventListener('click',resetScreen);
+fillBtn.addEventListener('click',colorScreen);
+options.addEventListener('change', resetScreen);
+pointSize.addEventListener('change',()=>{
+    screen.replaceChildren();//remove all points
+    generatePoints();
+});
