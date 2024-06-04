3480bdd07c1ea2888188010243c3ee828b1465c7
Salwa-mansour <alqadasisal@gmail.com>
2024-06-04 16:17:39 +0300

    Oparting calucaltion -- proplem with the firest calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -15,45 +15,52 @@
 // ===============
 
 const mainContainer = document.querySelector('.calculator');
-const display = mainContainer.querySelector('.calculator__display');
+const display = mainContainer.querySelector('.calculator__display input');
+const dispalyOprations = mainContainer.querySelector('.calculator__display .oprations')
 const keys = mainContainer.querySelectorAll('[class^=key]');
 const clearBtn = mainContainer.querySelector('#clear');
 const equalBtn = mainContainer.querySelector('#equal');
-
-let operatorClicked = false;
 let startNum = false ;
 let result = 0;
 let numCounter = 0;
-let mathExpression = {
-    // you can change thet to array of numbers and array of oprators if you want to perform multiple oprations in one shot 
-    numbers:[] ,
-    operations:[] ,
+let   numbers=[] ;
+let operations=[] ;
+
+
+function operate(){
+    let idxOpratro ;
+    let oprator ;
+    let idxNumber ;
+    let fNum ;
+    if(numbers.length ===1){
+        //set inial value
+        result = numbers[0];
+    }else{
+     idxOpratro = operations.length-1;
+     oprator = operations[idxOpratro];
+     idxNumber = numbers.length-1;
+     fNum = numbers[idxNumber];
+      
     
- }
+        switch (oprator){
+            case '+':
+                add(result,fNum);
+            break;
+            case '-':
+                subtract(result,fNum);
+            break;
+            case '*':
+                multiply(result,fNum);
+            break;
+            case '/':
+                divide(result,fNum);
+            break;
 
-function operate(result,num,operation){
-    switch (operation){
-        case '+':
-            add(result,num);
-        break;
-        case '-':
-            subtract(result,num);
-        break;
-        case '*':
-            multiply(result,num);
-        break;
-        case '/':
-            divide(result,num);
-        break;
-
+        }
     }
-    displayResult();
+ //   displayResult();
 }
-function displayResult(){
-     reset();
-    display.value = result;
-   
-}
+
  function add(num1,num2){
   result = parseFloat(num1) + parseFloat(num2) ;
  }
@@ -66,92 +73,44 @@ function displayResult(){
  function divide(num1,num2){
     result = parseFloat(num1)  / parseFloat(num2);
  }
- function getData(e){
- 
-    // seperate data from opration and call other funcions to oprate them
-    if(this.dataset.action ){
-        // handle previus opration result value
-    //   if(result !=="") mathExpression.num1 = result;
-    // && operatorClicked === false 
-        mathExpression.operations.push(this.dataset.action);
-        operatorClicked = true ;
-      
-     
-    }else{ 
-        let num = this.innerText;
-        if(!operatorClicked){
-        //   num = display.value + this.innerText ;
-          mathExpression.numbers[numCounter] = mathExpression.numbers[numCounter] + this.innerText;
-        }else{
-            num =  this.innerText ;
-            mathExpression.numbers.push(num);
-            numCounter++;
-        }
-        
-        
-        operatorClicked = false ;      
-    }
-    console.log({numCounter});
-    console.log(mathExpression)
-    updateDisplay(e,numCounter);
-    return mathExpression;
 
- }
  function getDataArr(e){
    if(this.dataset.action && !startNum){
-    mathExpression.operations.push(this.innerText);
-  //   operatorClicked = true ; 
+    operations.push(this.innerText);
      startNum = true;
-    // console.log(mathExpression.operations.length)
-    if(mathExpression.numbers.length ===1){
-        //set inial value
-        result = mathExpression.numbers[0];
-    }else{
-       let idxOpratro = mathExpression.operations.length-1;
-       let oprator = mathExpression.operations[idxOpratro];
-       let idxNumber = mathExpression.numbers.length-1;
-       let fNum = mathExpression.numbers[idxNumber];
-        operate(result,fNum,oprator)
-    
-    
-        console.log({result,fNum,oprator});
-    }
-        console.log(mathExpression)
-    
-   // console.log('path1')
-   }else{
-        if(startNum && !this.dataset.action  || mathExpression.numbers.length === 0 && !this.dataset.action ){
-            mathExpression.numbers.push(this.innerText);
+     displayExpretion(this.innerText);
+     operate();
+     displayResult();
+     console.log(result)
+   }else  if(startNum && !this.dataset.action  || numbers.length === 0 && !this.dataset.action ){
+            numbers.push(this.innerText);
             startNum = false;
-         //   console.log('path2')
-         
-        }else {
-          //  console.log('path3')
-        //  mathExpression.n  =
-       // console.log(mathExpression.numbers.length)
-        mathExpression.numbers[mathExpression.numbers.length - 1] =
-        mathExpression.numbers[mathExpression.numbers.length - 1] + this.innerText;
-    }
-   
+            displayExpretion(this.innerText);
+
+        }else if(!startNum && !this.dataset.action) {
+        numbers[numbers.length - 1] =
+        numbers[numbers.length - 1] + this.innerText;
+    
+        displayExpretion(this.innerText)
    }
-   displayExpretion(this.innerText)
+   
+    console.log({numbers,operations})
+  
  }
+
+
  function displayExpretion( item){
-   display.value = display.value + item;
+   dispalyOprations.innerText = dispalyOprations.innerText + item;
  }
  
- function updateDisplay(e,i){
-    display.value = `${mathExpression.numbers}${mathExpression.operations}`;
+ function displayResult(){
+    display.value = result;
  }
  function reset(){
     display.value = "";
-    mathExpression = {
-        numbers:[] ,
-        operations:[] ,
+    numbers = [] ;
+    operations = [] ;
         
-     }
-     operatorClicked = false;
-
  }
 
 
